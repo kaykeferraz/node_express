edd49f0ccc73a980e18195cd6eba60c05da09e99
EA - Kayke Ferraz De Moraes <kayke_ferraz@educadventista.org>
2023-10-30 15:11:01 -0300

    recebendo parametro na rota

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app =express()
 
 const caminhoBasse = path.join(__dirname,"templates")
 
+app.get('/Usuarios/:id', (requisicao, resposta) => {
+
+    const id = requisicao.params.id
+
+    console.log('Acessando dados de usuarios ${id}')
+
+    resposta.sendFile(`${caminhoBasse}/Usuarios.html`)
+})
+
 app.get('/',(requisicao,resposta)=> {
     resposta.sendFile(`${caminhoBasse}/index.html`)
   

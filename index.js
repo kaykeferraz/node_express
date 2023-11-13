0ffcc73c81addd2835bf368c4f7797ccdd32e593
EA - Kayke Ferraz De Moraes <kayke_ferraz@educadventista.org>
2023-11-13 14:49:54 -0300

    recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const app =express()
 
 const caminhoBasse = path.join(__dirname,"templates")
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
 app.post('/cadastrar/salvar', (requisicao,resposta) =>{
-    console.log(requisicao,body)
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`O email do usuário é: ${email}`)
 })
 
 app.get('/cadastrar', (requisicao, resposta) =>{
